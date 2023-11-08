dfaa3af43564c92618662e2dfe45166bc73f1e04
Kamal El Alami <kelalami123@gmail.com>
2023-11-08 16:40:32 +0100

    fix betty

--- tracklines.c
+++ tracklines.c
@@ -1,23 +1,31 @@
 #include "main.h"
 
-char *track_lines(void)
+/**
+ * track_lines - Read and track input lines
+ *
+ * This function reads input lines from the user, tracks their content, and
+ * returns the input as a dynamically allocated string.
+ *
+ * Return: A dynamically allocated string containing the user's input.
+ */
+char	*track_lines(void)
 {
-    char *line;
-    size_t len;
-    ssize_t llen;
+	char	*line;
+	size_t	len;
+	ssize_t	llen;
 
-    line = NULL;
-    len = 0;
-    if (isatty(STDIN_FILENO))
-    {
-        write(STDOUT_FILENO, "$ ", 2);
-    }
-    llen = getline(&line, &len, stdin);
+	line = NULL;
+	len = 0;
+	if (isatty(STDIN_FILENO))
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+	}
+	llen = getline(&line, &len, stdin);
 
-    if (llen == -1)
-    {
-        free(line);
-        return (NULL);
-    }
-    return (line);
+	if (llen == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
 }
