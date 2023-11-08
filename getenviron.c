ccfab05cd0f7c7db7f195536db24b6b2458caae1
Kamal El Alami <kelalami123@gmail.com>
2023-11-08 16:39:04 +0100

    add environ

---
+++ getenviron.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _getenviron - Get environment variable value
+ *
+ * This function searches for an environment variable by name and returns
+ * its value as a dynamically allocated string.
+ *
+ * @cmd: The name of the environment variable to retrieve.
+ *
+ * Return: The value of the environment variable, or NULL if not found.
+ */
+
+
+char	*_getenviron(char *cmd)
+{
+	char *strt, *val, *targ, *temp;
+	int i;
+
+	i = 0;
+	while (environ[i])
+	{
+		temp = _strdup(environ[i]);
+		strt = strtok(temp, "=");
+		if (_strcmp(strt, cmd) == 0)
+		{
+			val = strtok(NULL, "\n");
+			targ = _strdup(val);
+			free(temp);
+			return (targ);
+		}
+		free(temp), temp = NULL;
+		i++;
+		}
+	return (NULL);
+}
